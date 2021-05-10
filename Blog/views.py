ee7caafd09c50e4d9b2c9b14d1531bcbfee1e14f
Hernan <hgonzalez@tiendamia.com>
2021-05-10 03:33:25 -0300

    Improving functionality

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
 from .models import Page, Post, BaseUser, Logger
+import json
 import logging
 import inspect
 
@@ -28,10 +30,32 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def details(request: HttpRequest, post_id: int):
     context = {}
+    page = Page.objects.get(name='Details')
     post = Post.objects.get(pk=post_id)
     context['item'] = post
+    context['page_data'] = page
     return render(request, 'blog/details.html', context)
+
+
+@csrf_protect
+def delete(request: HttpRequest, post_id: int):
+    if request.method == 'POST' and request.headers['X-Csrftoken'] and 'csrftoken' in request.headers['Cookie']:
+        Post.objects.get(pk=post_id).delete()
+        print(post_id)
+        return JsonResponse({
+            'success': 'true',
+            'message': 'The post has been deleted!'
+        })
+    return JsonResponse({'res': 'YES'})
 
 
 def render_4xx(request: HttpRequest, message: str = 'The request could not be processed, please try again later.') -> HttpResponse:
     return render(request, 'error/4xx_error.html', {'message': message})
+
+
+def decode_request_body(request_body):
+    return decoded_request_to_string(request_body.decode('utf-8'))
+
+
+def decoded_request_to_string(decoded_request):
+    return decoded_request.replace('"', '')
